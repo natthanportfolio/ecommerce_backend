4eab9916d3d0fc1394704e9007ead63e371d316c
natthanportfolio <natthan.portfolio@gmail.com>
2021-11-08 17:20:07 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const expressValidator = require('express-validator');
+require('dotenv').config();
+// import routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+
+const orderRoutes = require('./routes/order');
+
+// app
+const app = express();
+
+// db
+URL=process.env.DATABASE
+mongoose.connect(URL, {
+
+    useNewUrlParser: true, 
+    
+    useUnifiedTopology: true 
+    
+    }, err => {
+    if(err) throw err;
+    console.log('Connected to MongoDB!!!')
+    });
+mongoose.set('useCreateIndex', true)
+
+
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+
+// routes middleware
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', productRoutes);
+
+app.use('/api', orderRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
